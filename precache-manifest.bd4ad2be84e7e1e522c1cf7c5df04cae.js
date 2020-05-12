afd5c603639a31bf501a7a126cfe9a6dacafb2fd
Ajith55 <nsajithkumar17@gmail.com>
2020-05-12 16:51:31 +0530

    Updates

---
+++ precache-manifest.bd4ad2be84e7e1e522c1cf7c5df04cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36b77f29098d0e20ef730b87b1e171a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bcde50247c8ebeea2b6d",
+    "url": "/monsters-rolodex/static/css/main.b8be92c1.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcde50247c8ebeea2b6d",
+    "url": "/monsters-rolodex/static/js/main.9ac46dc6.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
